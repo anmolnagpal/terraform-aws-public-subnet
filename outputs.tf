f9e91023869ac39ead685ec7caba56879a8c817b
Anmol Nagpal <ianmolnagpal@gmail.com>
2018-12-05 14:09:37 +0100

    update

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,11 @@
-output "az_subnet_ids" {
-  value       = "${zipmap(var.availability_zones, coalescelist( aws_subnet.public.*.id))}"
-  description = "Map of AZ names to subnet IDs"
+output "public_az_subnet_ids" {
+  value = "${module.public_subnets.az_subnet_ids}"
 }
 
-output "az_route_table_ids" {
-  value       = "${zipmap(var.availability_zones, coalescelist( aws_route_table.public.*.id))}"
-  description = " Map of AZ names to Route Table IDs"
+output "public_az_ngw_ids" {
+  value = "${module.public_subnets.az_ngw_ids}"
 }
 
-output "az_ngw_ids" {
-  value       = "${zipmap(var.availability_zones, coalescelist(aws_nat_gateway.public.*.id, local.dummy_az_ngw_ids))}"
-  description = "Map of AZ names to NAT Gateway IDs (only for public subnets)"
+output "public_az_route_table_ids" {
+  value = "${module.public_subnets.az_route_table_ids}"
 }
